848b609639a9ca333cfa2af09ac1487aeeb846e8
mbaptist <mbaptist@5bac3ec7-752e-0410-9700-c12c62b75ab7>
2006-09-19 17:00:04 +0000

    began adding raw save/load

---
+++ io.C
@@ -0,0 +1,103 @@
+//#include "vtkio.h"
+#include <string>
+#include <iostream>
+#include <fstream>
+
+#include <cat.h>
+using namespace std;
+using namespace cat;
+
+template <class T>
+void vtkFileLoad(const std::string & vtkfilename,T & data)
+{
+	//temporary strings
+	string stmp;
+	//input stream
+	ifstream ifs((vtkfilename+".vtk").c_str());
+  //read file header
+	ifs >> stmp;ifs >> stmp;ifs >> stmp;ifs >> stmp;ifs >> stmp;
+	ifs >> stmp;
+	ifs >> stmp;
+	ifs >> stmp;ifs >> stmp;
+	cat::tvector<int,3> dimensions;
+	ifs >> stmp;
+	ifs >> dimensions;
+	ifs >> stmp;ifs >> stmp;ifs >> stmp;ifs >> stmp;
+	cout << stmp << endl;
+	ifs >> stmp;ifs >> stmp;ifs >> stmp;ifs >> stmp;
+	cout << stmp << endl;
+	ifs >> stmp;ifs >> stmp;
+	cout << stmp << endl;
+	string fieldtype;
+	ifs >> fieldtype;
+	cout << fieldtype << endl;
+	ifs >> stmp;ifs >> stmp;
+	cout << stmp << endl;
+	if (fieldtype=="SCALARS")
+	{
+		ifs >> stmp;
+		cout << stmp << endl;
+		ifs >> stmp;
+		cout << stmp << endl;
+	}
+	//read data
+	int n1=dimensions[0];
+	int n2=dimensions[1];
+	int n3=dimensions[2];
+	T data_read(n1,n2,n3);
+	//cout << dimensions << endl;
+	for(int k=0;k<n3;++k)
+		for(int j=0;j<n2;++j)
+			for(int i=0;i<n1;++i)
+			{
+				ifs >> data_read(i,j,k);
+				//cout << data_read(i,j,k) << endl;
+			}
+	data=data_read;
+}
+
+
+template <class T>
+void vtkFileSave(const std::string & vtkfilename,const T & data,cat::tvector<double,3> _vtkfile_origin_,cat::tvector<double,3> _vtkfile_spacing_,std::string _vtkfile_fieldname_)
+{
+   //output stream
+	ofstream ofs((vtkfilename+".vtk").c_str());
+	//Header data
+	std::string vtkfile_version="2.0";
+	std::string vtkfile_creator=".";
+	std::string vtkfile_encoding="ASCII";
+	std::string	vtkfile_datasettype="STRUCTURED_POINTS";
+	cat::tvector<int,3>	vtkfile_dimensions=data.shape();
+	int	vtkfile_size=data.size();
+	cat::tvector<double,3> vtkfile_origin=_vtkfile_origin_;
+	cat::tvector<double,3> vtkfile_spacing=_vtkfile_spacing_;
+	std::string vtkfile_fieldtype;
+	if (vtkFileTraits<T>::fieldtype==0)
+			vtkfile_fieldtype="SCALARS";
+		else
+			vtkfile_fieldtype="VECTORS";
+	std::string vtkfile_fieldname=_vtkfile_fieldname_;
+	std::string vtkfile_numerictype="float";
+   //write file header
+	ofs << "# vtk DataFile Version " << vtkfile_version << "\n"
+		<< vtkfile_creator << "\n"
+		<< vtkfile_encoding	<< endl;
+	//write dataset header
+	ofs << "DATASET " << vtkfile_datasettype << "\n"
+		<< "DIMENSIONS " << vtkfile_dimensions << "\n"
+		<< "ORIGIN " << vtkfile_origin << "\n"
+		<< "SPACING " << vtkfile_spacing << "\n"
+		<< "POINT_DATA " << vtkfile_size << "\n"
+		<< vtkfile_fieldtype << " " << vtkfile_fieldname << " " << vtkfile_numerictype << endl;
+	if (vtkfile_fieldtype=="SCALARS")
+		ofs << "LOOKUP_TABLE default" << endl;
+	//write data
+	int n1=vtkfile_dimensions[0];
+	int n2=vtkfile_dimensions[1];
+	int n3=vtkfile_dimensions[2];
+	for(int k=0;k<n3;++k)
+		for(int j=0;j<n2;++j)
+			for(int i=0;i<n1;++i)
+				ofs << data(i,j,k) << endl;
+}
+
